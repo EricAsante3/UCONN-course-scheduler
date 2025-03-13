4b79f0cadabd26bb33641faae13ef510611f97a5
Eric Asante <3ericasante@gmail.com>
2025-03-13 17:49:09 -0400

    cleaned repo

--- testing_files/json_printer.py
+++
@@ -1,6 +0,0 @@
-import json
-
-def json_printer(courses, file_name):
-    """Writes json to a .txt file""" 
-    with open(('./' + file_name + '.txt'), 'w') as file:
-            json.dump(courses, file, indent=4)
