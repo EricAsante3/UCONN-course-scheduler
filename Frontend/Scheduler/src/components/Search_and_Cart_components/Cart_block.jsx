a21e149f13d82956330b1e630c455d16c4c737fc
ethant0123 <ethan.thomas@uconn.edu>
2025-03-15 02:25:27 -0400

    Initial design of class list

--- Frontend/Scheduler/src/components/Search_and_Cart_components/Cart_block.jsx
+++ Frontend/Scheduler/src/components/Search_and_Cart_components/Cart_block.jsx
@@ -3,8 +3,23 @@
 
 function Cart_block() {
 
+    // const items = ["Apple", "Banana", "Cherry"];
+    const items = ["CSE 1010", "CSE 2050", "CSE 3150"]
+
     return (
-        <div className="h-[50rem]  bg-green-500 size-full">cart block</div>
+        <div className="h-[50rem] flex flex-col justify-top items-center bg-green-500 size-full">
+            {/* <div className="h-[80%] w-[80%] flex bg-red-500 size-full">
+                playtime is over
+            </div> */}
+            <h1 className="text-5xl m-20">Class List</h1>
+            <div className="h-[80%] mt-auto border-2 size-full">
+                <ul>
+                    {items.map((item, index) => (
+                        <li key={index} className="text-3xl border-2 p-4">{item}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
     )
   }
   
