b2d27e8afcacc77beb56f285577ea19565566d26
ethant0123 <ethan.thomas@uconn.edu>
2025-03-21 21:03:13 -0400

    new register classes functionality added

--- Frontend/Scheduler/src/page2/display.jsx
+++ Frontend/Scheduler/src/page2/display.jsx
@@ -1,17 +1,21 @@
 
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { DataContext } from "../data/data";
 import CalendarApp from "./Viewer_pop_up/Calender";
 import Classs_card from "./class_card";
 import { useNavigate } from "react-router-dom";
 import { ArrowLeftIcon } from '@heroicons/react/24/outline';
 import { ShoppingCartIcon } from '@heroicons/react/24/outline';
+import { Button, Modal, Box} from "@mui/material";
+import Register_classes from '/src/page2/Register_classes.jsx';
+
 
 
 function display() {
     const {temp_schedule,temp_events} = useContext(DataContext);
     console.log(temp_schedule)
     const navigate = useNavigate();
+    const [open, setOpen] = useState(false);
 
     const handleClick = () => {
         navigate("/");
@@ -38,15 +42,44 @@ function display() {
             </div>
             <h1 className="text-4xl p-2 font-bold text-white">Schedule {temp_events["index"]}</h1>
 
-            <div className=" cursor-pointer h-full flex border-2 flex-col justify-center items-center border-black p-2 text-white rounded-xl">
+            <div onClick={() => setOpen(true)} className=" cursor-pointer h-full flex border-2 flex-col justify-center items-center border-black p-2 text-white rounded-xl">
                 <ShoppingCartIcon className="text-white"></ShoppingCartIcon>
-                <button className="flex text-white" onClick={handleClick}>Register Classes</button>
+                <button className="flex text-white" >
+                    Register Classes
+                </button>
+
             </div>
         </div>
+
+        <Modal open={open} onClose={() => setOpen(false)}>
+            <Box
+                sx={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                height: "75%",
+                width: "80%",
+                bgcolor: "background.paper",
+                borderRadius: 2,
+                boxShadow: 24,
+                p: 4,
+                maxHeight: "90vh",
+                overflowY: "auto",
+                }}
+            >
+                <Box sx={{  display: 'flex', flexDirection: 'column'}}>
+                    <Register_classes classInfo={temp_schedule} />
+                    <Button onClick={() => setOpen(false)} sx={{ mt: 'auto', mr: 'auto'}} variant="contained">
+                    Close
+                    </Button>
+                </Box>
+            </Box>
+        </Modal>
+
         <CalendarApp schedule_info={temp_events}></CalendarApp>
 
         </div>
-
 
     </div>
 
