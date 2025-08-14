322802aab52b87c10e4d295954c2d3855015c715
Eric Asante <3ericasante@gmail.com>
2025-08-13 22:19:15 -0400

    rough template complete

---
+++ testing/MyApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using DataObjects;
+
+public class Program
+{
+
+    public static void Main()
+    {
+        // Read the file
+        string filePath = "output.json";
+        string jsonContent = File.ReadAllText(filePath);
+
+        // Deserialize
+        RootPayload data = JsonSerializer.Deserialize<RootPayload>(jsonContent);
+
+        // Output to verify
+        data.InitializeScheduler();
+        data.value["ECE 2001"].Test();
+
+        
+        string json = JsonSerializer.Serialize(data.Scheduler(), new JsonSerializerOptions { WriteIndented = true });
+        Console.WriteLine(json);
+
+    }
+
+
+
+    
+
+}
+
+
+
