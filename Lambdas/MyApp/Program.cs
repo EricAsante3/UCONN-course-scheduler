34ba64951e747b83f794d540c7cb2273467d64d3
Eric Asante <3ericasante@gmail.com>
2025-08-18 21:05:41 -0400

    Lambdas v1

---
+++ Lambdas/MyApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using DataObjects;
+using Amazon.Lambda.Core;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+
+namespace MySchedulerLambda
+{
+    public class SchedulerFunction
+    {
+        public static string SchedulingFunction(SchedulingSystem SchedulingSystem, ILambdaContext context)
+        {
+            SchedulingSystem.InitializeScheduler();
+            string json = JsonSerializer.Serialize(SchedulingSystem.Schedule(), new JsonSerializerOptions { WriteIndented = true });
+            return json;
+        }
+    }
+}
+
+
---
+++ Lambdas/MyApp/SchedulingObjects/DataObjects.cs
@@ -0,0 +1,63 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text.Json;
+using System.Xml;
+
+namespace DataObjects
+{
+    public class CourseEntry
+    {
+        public required string CourseName;
+        public required List<string> AllClassCRNs { get; set; }
+        public required List<SectionEntry> SectionEntries { get; set; }
+        public required Dictionary<string, Dictionary<string, HashSet<string>>> ConflictDetectionHashMap;
+
+        public CourseEntry()
+        {
+        }
+
+        public Dictionary<string, object> AddConflictingSection(string conflictCourse, string conflictCrn, string ownCrn)
+        {
+            if (!ConflictDetectionHashMap[conflictCourse][conflictCrn].Contains(ownCrn))
+            {
+                ConflictDetectionHashMap[conflictCourse][conflictCrn].Add(ownCrn);
+
+                if (ConflictDetectionHashMap[conflictCourse][conflictCrn].Count == this.AllClassCRNs.Count)
+                {
+                    ConflictDetectionHashMap[conflictCourse]["conflictCRNs"].Add(conflictCrn);
+
+                    if (ConflictDetectionHashMap[conflictCourse]["conflictCRNs"].Count == (ConflictDetectionHashMap[conflictCourse].Count - 1))
+                    {
+                        Console.WriteLine("All class section time conflict between" + this.CourseName + conflictCourse);
+                        
+                        return new Dictionary<string, object>
+                        {
+                            { "terminate", true },
+                            { "message", "All class section time conflict between" + this.CourseName + conflictCourse }
+                        };
+
+                    }
+                    return new Dictionary<string, object>
+                    {
+                        { "terminate", false },
+                        { "message", "" }
+                    };
+                }
+            }
+
+            return new Dictionary<string, object>
+            {
+                { "terminate", false },
+                { "message", "" }
+            };
+        }
+
+
+        public bool CheckConfilict(string conflictClassName, string conflictCrn, string ownCrn)
+        {
+            return ConflictDetectionHashMap[conflictClassName][conflictCrn].Contains(ownCrn);
+        }
+    }
+}
+
