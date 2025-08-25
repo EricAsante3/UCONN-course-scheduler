e936f9099bed9f1f1b102875626de0bbac5d7ff6
Eric Asante <3ericasante@gmail.com>
2025-08-25 08:44:33 -0400

    Revert "hope2"
    This reverts commit f67dbdb7f39f82ea4e78316f95b01fcc205a418e.

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    basePath: '/doingmythang',
-    assetPrefix: '/doingmythang/',
-    trailingSlash: true
+
 };
 
 export default nextConfig;
